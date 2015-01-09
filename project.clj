90582b41e7020462041662f8ca8df359fffb33a5
ipavl <ipavl@users.sourceforge.net>
2015-01-08 21:41:57 -0500

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/java.jdbc "0.3.6"]
                  [java-jdbc/dsl "0.1.1"]
                  [mysql/mysql-connector-java "5.1.34"]
-                 [clj-time "0.8.0"]
+                 [clj-time "0.9.0"]
                  [lib-noir "0.9.5"]]
   :plugins [[codox "0.8.10"]]
   :codox {:src-dir-uri "https://github.com/ipavl/clorum-core/blob/master/"
